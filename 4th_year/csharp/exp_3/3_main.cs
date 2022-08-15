18355f9c29d7f7febacb1fa2a45766cb47784924
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2022-08-16 00:40:25 +0530

    Main Logic done

--- 4th_year/csharp/exp_3/3_main.cs
+++ 4th_year/csharp/exp_3/3_main.cs
@@ -1,3 +1,4 @@
+//operator overloading Oppenheimer
 using System;
 namespace OperatorOverloading
 {
@@ -19,10 +20,7 @@ namespace OperatorOverloading
             temp.img = c1.img + c2.img;
             return temp;
         }
-        public void Display()
-        {
-            Console.WriteLine($"{real} + i{img}");
-        }
+        
         //
         // " - "  subtraction opertor
         public static complex operator -(complex c1, complex c2)
@@ -32,52 +30,44 @@ namespace OperatorOverloading
             temp.img = c1.img - c2.img;
             return temp;
         }
-        public void Display()
-        {
-            Console.WriteLine($"{real} + i{img}");
-        }
+        
         //
         // " / " Divison opertor overloading
         public static complex operator /(complex c1, complex c2)
         {
             complex temp = new complex();
-            if (c2.real && c2.img !=0){
+            //if ((c2.real && c2.img) != 0){
                 temp.real = c1.real / c2.real;
                 temp.img = c1.img / c2.img;
                 return temp;
-            }
-            else{
-
-            }          
+            //}
+                   
         }
-        public void Display()
-        {
-            Console.WriteLine($"{real} + i{img}");
-        }
+       
         //
         // " * " opertor
         public static complex operator *(complex c1, complex c2)
         {
             complex temp = new complex();
-            if (c1.real||c2.real||c2.img||c2.img !=0){
+            //if (c1.real||c2.real||c2.img||c2.img !=0){
                 temp.real = c1.real * c2.real;
                 temp.img = c1.img * c2.img;
                 return temp;
-            }
-            else{
+            //}
+            /* else{
                 temp.real=0;
                 temp.img=0;
-            }
+            } */
                 
         }
         public void Display()
         {
-            if(real||img!=0){
+            //if(real||img!=0){
                 Console.WriteLine($"{real} + i{img}");
-            }
-            else{
-                Console.WriteLine(0);
-            }
+            //}
+            //else{
+                //Console.WriteLine(0);
+            //}
         }
     };
 
@@ -86,14 +76,20 @@ namespace OperatorOverloading
         static void Main(string[] args)
         {
             complex c1 = new complex(3, 7);
-            c1.Display();
+          
             complex c2 = new complex(5, 2);
-            c2.Display();
-            complex c3 = c1 + c2;
-            c3.Display();
-            Console.ReadKey();
+            
+            complex c4 = c1 + c2;
+            c4.Display();
+            complex c5 = c1 - c2;
+            c5.Display();
+            complex c6 = c1 * c2;
+            c6.Display();
+            complex c7 = c1 / c2;
+            c7.Display();
+            
         }
     }
+}
 
     
-}
