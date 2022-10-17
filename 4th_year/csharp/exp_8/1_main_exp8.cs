623a9e351b3103d92416c7378f2b777b9cc0b6e7
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2022-10-18 01:27:30 +0530

    Jugaad Is Working

--- 4th_year/csharp/exp_8/1_main_exp8.cs
+++ 4th_year/csharp/exp_8/1_main_exp8.cs
@@ -3,20 +3,30 @@ using System;
 using System.Threading;
 class Program
 {
-
+    int amt = 2500;
     public void startthreads()
-    {
-        int ama = 1000;
-        Console.WriteLine("Enter the amount to be deposited");
-        Thread t1 = new Thread(new ParameterizedThreadStart(withdrawal));
-        t1.Name = "Withdraw Thread";
-        t1.Start(ama);
-    }
-    public void withdrawal(object mon)
     {
         Monitor.Enter(this);
         {
-            int amt = Convert.ToInt32(mon);
+
+            Thread t1 = new Thread(new ThreadStart(withdrawal));
+            t1.Name = "Withdraw Thread";
+            t1.Start();
+            Thread t2 = new Thread(new ThreadStart(deposit));
+            t2.Name = "Deposit Thread";
+            t2.Start();
+            Thread t3 = new Thread(new ThreadStart(withdrawal));
+            t3.Name = "Withdraw Thread";
+            t3.Start();
+        }
+        Monitor.Exit(this); 
+
+    }
+    public void withdrawal()
+    {
+        Monitor.Enter(this);
+        {
+            
             Console.WriteLine("{0} started", Thread.CurrentThread.Name);
             Console.WriteLine("Thread Priority:" + Thread.CurrentThread.Priority);
             Console.WriteLine("The amount to be withdrawn: ");
@@ -33,14 +43,31 @@ class Program
         Console.ReadKey();
         Monitor.Exit(this);
     }
+    public void deposit()
+    {
+        Monitor.Enter(this);
+        {
+
+            Console.WriteLine("{0} started", Thread.CurrentThread.Name);
+            Console.WriteLine("Thread Priority:" + Thread.CurrentThread.Priority);
+            Console.WriteLine("The amount to be Deposited ");
+            int bal;
+            bal = int.Parse(Console.ReadLine());
+            amt = amt + bal;
+            Console.WriteLine("The amount is {0}", amt);
+            
+        }
+        Console.ReadKey();
+        Monitor.Exit(this);
+    }
     static void Main(string[] args)
 
     {
         Console.WriteLine("Banking Application");
         Program p = new Program();
         p.startthreads();
-        
+
     }
-   
-    
+
+
 }
