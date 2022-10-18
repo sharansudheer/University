3591c8ba015017d5750a47128bf7537772414ad8
Sharan Sudheer <56349474+sharansudheer@users.noreply.github.com>
2022-10-18 15:14:29 +0530

    In progress, Java masala

--- 4th_year/csharp/exp_8/3_producer_prob.cs
+++ 4th_year/csharp/exp_8/3_producer_prob.cs
@@ -1,111 +1,117 @@
-//inital commit
+//"Das Leben ist am Ende nur eine Ansammlung von verpassten Chancen."
+
 using System;
 using System.Threading;
-
-public class MyData
-{
-    private double _pi = 0.0;
-    private int _iterations = 0;
-    private bool _valueReady = false;
-
-    public void WriteData(int iterations, double val)
-    {
-        lock (this)
-        {
-            if (_valueReady)
-            {
-                Monitor.Wait(this);
-            }
-            _pi = val;
-            _iterations = iterations;
-            _valueReady = true;
-            Monitor.Pulse(this);
-        }
-    }
-
-    public void ReadData(out int iterations, out double val)
-    {
-        lock (this)
-        {
-            if (!_valueReady)
-            {
-                Monitor.Wait(this);
-            }
-            val = _pi;
-            iterations = _iterations;
-            _valueReady = false;
-            Monitor.Pulse(this);
-        }
-    }
-}
-
-class MyProducer
-{
-    private MyData _pi;
-    private int TotalIters;
-
-    public MyProducer(MyData pi, int iterations)
-    {
-        _pi = pi;
-        TotalIters = iterations;
-    }
-
-    public Thread CreateProducerThread()
-    {
-        return new Thread(new ThreadStart(this.calculate));
-    }
-    private void calculate()
-    {
-        int iters = 1;
-        do
-        {
-            iters += 4;
-            _pi.WriteData(iters, iters * 4);
-        } while (iters < TotalIters);
-    }
-}
-
-class MyConsumer
-{
-    private MyData _pi;
-    private int TotalIters;
-
-    public MyConsumer(MyData pi, int iterations)
-    {
-        _pi = pi;
-        TotalIters = iterations;
-    }
-
-    public Thread CreateConsumerThread()
-    {
-        return new Thread(new ThreadStart(this.printValues));
-    }
-
-    private void printValues()
-    {
-        int iters = new int();
-        double pi = new double();
-        do
-        {
-            _pi.ReadData(out iters, out pi);
-            System.Console.WriteLine("Iters: {0}\tPi:  {1}", iters.ToString(), pi.ToString());
-        } while (iters < TotalIters);
-    }
-}
+using System.Collections;
+using System.Collections.Generic;
 
 class MainClass
 {
     static void Main(string[] args)
+        throw new ThreadInterruptedException();
     {
-        MyData pi = new MyData();
-        MyProducer prod = new MyProducer(pi, 100000);
-        Thread producerThread = prod.CreateProducerThread();
-        MyConsumer cons = new MyConsumer(pi, 100000);
-        Thread consumerThread = cons.CreateConsumerThread();
-        producerThread.Start();
-        consumerThread.Start();
+        
+        sealed Producer prod = new Producer();
 
-        producerThread.Join();
-        consumerThread.Join();
+    Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run()
+    {
+        try
+        {
+            pc.produce();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+});
+
+// Create consumer thread
+Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run()
+{
+    try
+    {
+        pc.consume();
+    }
+    catch (InterruptedException e)
+    {
+        e.printStackTrace();
+    }
+}
+        });
+
+// Start both threads
+t1.start();
+t2.start();
+
+// t1 finishes before t2
+t1.join();
+t2.join();
+    }
+
+
+
+
+
+
+public static class Producer
+{
+    LinkedList<int> list = new LinkedList<int>();
+    int capacity = 2;
+    public void produce() throws InterruptedException
+    {
+
+        while (true) {
+                synchronized(this)
+        {
+            // producer thread waits while list
+            // is full
+            while (list.size() == capacity)
+                wait();
+
+            System.out.println("Producer produced-"
+                               + value);
+
+            // to insert the jobs in the list
+            list.add(value++);
+
+            // notifies the consumer thread that
+            // now it can start consuming
+            notify();
+
+            // makes the working of program easier
+            // to  understand
+            Thread.sleep(1000);
+        }
+    }
+}
+
+// Function called by consumer thread
+public void consume() throws InterruptedException
+{
+            while (true) {
+        synchronized(this)
+                {
+            // consumer thread waits while list
+            // is empty
+            while (list.size() == 0)
+                wait();
+
+            // to retrieve the first job in the list
+            int val = list.removeFirst();
+
+            System.out.println("Consumer consumed-"
+                               + val);
+
+            // Wake up producer thread
+            notify();
+
+            // and sleep
+            Thread.sleep(1000);
+        }
     }
 }
